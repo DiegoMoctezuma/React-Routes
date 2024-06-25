9cdc399519a00b0c54d357b2aebc71d96de803e1
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-06-25 16:18:23 -0600

    Pagina final: React Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Page404 from './pages/Page404';
+import Header from './components/Header';
+import Post from './pages/Post';
+import Categoria from './pages/Categoria';
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="/posts/:id" element={<Post />} />
+        <Route path="/categoria/:id/*" element={<Categoria/>} />
+        <Route path='*' element={<Page404/>}/>
       </Routes>
     </Router>
   );
